c7c3a7290dee5bb527d1b040e01d86a6d07dff65
Stepan <kvantoreps@gmail.com>
2021-11-04 17:09:54 +0300

    feat(weather): add lang option to weather service

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,11 +1,13 @@
 const API_KEY = 'b6a21416b06ca6f193e4a8a25d7cef6a'
 
 export class Weather {
-  static async get(city) {
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`).then(x => x.json())
+  static async get(city, { lang }) {
+    const selectedLang = lang ?? 'ru'
+    const query = `q=${city}&lang=${selectedLang}&units=metric&appid=${API_KEY}`
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?${query}`).then(x => x.json())
 
     if (res.cod === '404') {
-      return null
+      return 'not_found'
     }
 
     return res
