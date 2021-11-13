87db6b15f525a475ba3eedce6f01d96e48eab001
Stepan <kvantoreps@gmail.com>
2021-11-13 21:23:11 +0300

    feat(config): add base url

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const path = require('path')
 const vueSrc = './src'
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/weather-app/'
+    : '/',
   transpileDependencies: [
     'vuetify'
   ],
