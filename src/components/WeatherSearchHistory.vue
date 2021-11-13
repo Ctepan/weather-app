524f9dd309d5bbce5c2065e6ad7dea7a06500f80
Stepan <kvantoreps@gmail.com>
2021-11-13 20:46:07 +0300

    feat(components): add search history component

---
+++ src/components/WeatherSearchHistory.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-list class="pa-0">
+    <v-list-item
+      :disabled="locked"
+      v-for="searchItem in searchItems"
+      :key="String(searchItem.date)"
+      @click="$emit('input', searchItem.query)"
+    >
+      {{ searchItem.query }}
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: 'WeatherSearchHistory',
+  props: {
+    searchItems: {
+      type: Array,
+      default: () => []
+    },
+    locked: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
